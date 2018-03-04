b915af37c12d97c40c003af0e15bfa288768c55f
Paula MacTavish <perilousp@hotmail.com>
2018-03-04 11:03:02 -0500

    more updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,22 @@ $(document).ready(function () {
 var correctCounter = 0;
 var incorrectCounter = 0;
 var unansweredCounter = 0;
-var question = 0;
+var qAndaCounter = 0;
 var triviaQuestions = ["Where is the driest place on Earth?", 
                       "Which is the busiest airport in the World?", 
                       "Worldwide, what is the most expensive city to live in?", 
                       "What is the longest river in the World?"];
-var answer = 0;
+var answers = [];
 var rightAnswers = ["Atacama Desert, Chile", "Hartsfield-Jackson International, Atlanta, GA", "Singapore", "Nile"];
-var wrong = 0;
+var wrongAnswerCounter = 0;
 var wrongAnswers = ["Sahara Desert, North Africa", "Gobi Desert, China and Mongolia", "Death Valley, California and Nevada, U.S.A.", 
                     "Heathrow Airport, London, England", "Charles de Gaulle Airport, Paris, France", "Narita International Airport, Tokyo, Japan", 
                     "Tokyo", "New York City", "London",
                     "Amazon", "Congo", "Yangtze"];
+var answersShuffled = [];
 var yourAnswer = "";
 var images = ["assets/images/atacamaDesert.jpg", "assets/images/atlantAirport.jpeg", "assets/images/singapore.jpg", "assets/images/nile.jpg"];
-var timeToPlay = 20
+var timeToPlay = 10
 var interval;
 
 
@@ -28,19 +29,54 @@ $("#start").click (function run() {
     $("#timerScreen").show();
     $("#questionScreen").show();
     $("#outcomeScreen").hide();
-    $("#scoreScreen").hide();  
+    $("#scoreScreen").hide();
+    
+    $("#question").text(triviaQuestions[qAndaCounter]);
+    console.log(triviaQuestions[qAndaCounter]);
+    
+    answers.push(rightAnswers[qAndaCounter]);
+    
+    // moves 3 answers to wrongAnswer array
+    for (var h = 0 + wrongAnswerCounter; h < (3+ wrongAnswerCounter); h++) {
+        answers.push(wrongAnswers[h]);
+        }
+
+    // Shuffle Possible Answers
+    if (answers.length = 4){        
+        //runs 4 times
+        for (var i = answers.length - 1; i >= 0; i--) {        
+        // pick a number between 0 and 3, next run 0 and 2, ect
+        var answerChosen = Math.floor(Math.random() * (i + 1)); 
+        // puts answer(index of random#) into answersShuffled array
+        answersShuffled.push(answers[answerChosen]);
+        // removes answerChosen from answers array
+        answers.splice([answerChosen], 1);
+        };    
+    };
+
+    
+    if (answersShuffled.length = 4) {
+        console.log(answersShuffled)
+        for (var j = 0; j < answersShuffled.length; j++); {
+        
+        // $("ans[j]").append(answersShuffled[j-1]);     
+        console.log(answersShuffled[j]);
+        
+        }
+    };
+    
 });
 
 function decrement() {
-   timeToPlay--
-    $("#question").text(triviaQuestions[question]);
-    console.log(triviaQuestions[question]);
+    timeToPlay--
     $("#time").text(timeToPlay);
     if (timeToPlay === 0) {
         clearInterval(interval);
         $("#outcome").text("You ran out of time!");
         $("#rightAnswer").text("XXXXXXX");
-        // $("#images").html(<img "src='\" +  images[0]  + \" '" >);
+        // $("#images").prepend("<img src=""+images[0]+"">"images[0]">");
+        // $('').append("<div><img src='"+el[0]+"'>"+el[1]+"</div>");             
+        
         $("#questionScreen").hide();
         $("#outcomeScreen").show();
         
@@ -50,12 +86,7 @@ function stopPlay() {
     clearInterval(intervalId);
 }
       
-run();
-}
 
-
-function myStopFunction() {
-    clearTimeout(guess);
 }
 
 });
